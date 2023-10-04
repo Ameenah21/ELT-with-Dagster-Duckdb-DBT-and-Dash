5420e276562d39efb9b59a9937ae4c6a84168c58
Ameenah21 <aminatlawal21@gmail.com>
2023-10-04 08:09:17 +0100

    Syntax Error: Edited Indentation error in asset.py

--- sportsdata/sportsdata/assets.py
+++ sportsdata/sportsdata/assets.py
@@ -6,6 +6,7 @@ from dagster_duckdb import DuckDBResource
 from dagster import Definitions
 import os
 
+
 @asset
 def league_standing():
     urls = [
@@ -23,32 +24,33 @@ def league_standing():
         url = url_info["url"]
         source = url_info["source"]
 
-    # Send HTTP Request and Parse HTML
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
+        # Send HTTP Request and Parse HTML
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "lxml")
 
-    # Find and Extract Table Headers
-    table = soup.find("table", class_="standing-table__table")
-    headers = table.find_all("th")
-    titles = [i.text for i in headers]
+        # Find and Extract Table Headers
+        table = soup.find("table", class_="standing-table__table")
+        headers = table.find_all("th")
+        titles = [i.text for i in headers]
 
-    # Create an Empty DataFrame
-    df = pd.DataFrame(columns=titles)
+        # Create an Empty DataFrame
+        df = pd.DataFrame(columns=titles)
 
-    # Iterate Through Table Rows and Extract Data
-    rows = table.find_all("tr")
-    for i in rows[1:]:
-        data = i.find_all("td")
-        row = [tr.text.strip() for tr in data]  # Apply .strip() to remove \n
-        l = len(df)
-        df.loc[l] = row
+        # Iterate Through Table Rows and Extract Data
+        rows = table.find_all("tr")
+        for i in rows[1:]:
+            data = i.find_all("td")
+            row = [tr.text.strip() for tr in data]  # Apply .strip() to remove \n
+            l = len(df)
+            df.loc[l] = row
 
-    # Add a column for source URL
-    df["Source"] = source
+        # Add a column for source URL
+        df["Source"] = source
 
-    # Append the DataFrame to the list
-    dfs.append(df)
+        # Append the DataFrame to the list
+        dfs.append(df)
 
     # Concatenate all DataFrames into a single DataFrame
     football_standing = pd.concat(dfs, ignore_index=True)
     football_standing.to_csv("footballstanding.csv")
+
